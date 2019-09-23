13f754e4d05f6e75acfef9ee0680cf5dd7da2559
Simeone <simeoneumoh@gmail.com>
2019-09-23 16:51:54 +0100

    First Commit

---
+++ prac.py
@@ -0,0 +1,24 @@
+v = 'sim'
+print(v.lower().endswith('m'))
+
+
+r = 'My name is Calisto'
+print(r.lower())
+
+s = 'sim'
+a = bool(r.startswith('s'))
+
+num=0o52
+print(num)
+
+
+b = bool(num)
+print(b)
+
+data='hello'
+
+l = list(data)
+print(l)
+
+g = set(data)
+print(g)
