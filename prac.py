1d375ce171c2a1d08c227e918c5ffb3707e1ac82
Simeone <simeoneumoh@gmail.com>
2019-10-02 18:27:41 +0100

    Dictionaries and Set Operators

--- prac.py
+++ prac.py
@@ -26,3 +26,22 @@ best = 'cristianoronaldo'
 print(best[0:14:2])
 goat='leomessi'
 print(bool([6,7,15]>[6,8,11]))
+
+d = 'leo'
+tea = set(d)
+print(tea)
+
+print(bool(tea==g))
+print(bool(tea<=g))
+print(bool(tea<g))
+print(bool(tea>g))
+print(tea&g)
+print(tea^g)
+
+player = {
+    'name':'Cristiano',
+    'club':'Juventus',
+    'age': 34,
+}
+for key,value in player.items():
+    print("The first player is " + player['name']+" He plays for " + player['club'] + " He is " + str(player['age']))
